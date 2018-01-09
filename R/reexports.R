e59d23a1a6b480162bc09126da053eb79e20683a
alfonso <alfonso.r.reyes@gmail.com>
2018-01-09 09:37:19 -0600

    add some reticulate functionsthat will be used often

---
+++ R/reexports.R
@@ -0,0 +1,31 @@
+
+
+#' @export
+reticulate::import
+
+#' @export
+reticulate::dict
+
+#' @export
+reticulate::tuple
+
+#' @export
+reticulate::np_array
+
+#' @export
+reticulate::array_reshape
+
+#' @export
+reticulate::iterate
+
+#' #' @export
+#' reticulate::`%as%`
+#'
+#' #' @export
+#' reticulate::use_python
+#'
+#' #' @export
+#' reticulate::use_virtualenv
+#'
+#' #' @export
+#' reticulate::use_condaenv
