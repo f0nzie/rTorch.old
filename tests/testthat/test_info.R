a83301e3f2ceeff6da5fcf1afd784a703bb8a7e1
f0nzie <alfonso.reyes@oilgainsanalytics.com>
2019-07-18 18:52:34 -0500

    info version

---
+++ tests/testthat/test_info.R
@@ -0,0 +1,20 @@
+library(testthat)
+
+source("tensor_functions.R")
+
+
+context("PyTorch version")
+
+test_that("PyTorch version is 1.1.0", {
+    expect_equal(torch$`__version__`, "1.1.0")
+})
+
+
+test_that("CUDA is not available", {
+    expect_equal(torch$cuda$is_available(), FALSE)
+})
+
+test_that("Number of CPUs", {
+    expect_equal(torch$get_num_threads(), 4)
+})
+
