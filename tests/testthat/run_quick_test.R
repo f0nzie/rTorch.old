3506a145f77ec115b2596346f7571d8d1f232c8d
f0nzie <alfonso.reyes@oilgainsanalytics.com>
2019-07-23 19:02:56 -0500

    run quick test

--- tests/testthat/run_quick_test.R
+++ tests/testthat/run_quick_test.R
@@ -12,3 +12,28 @@ tensor_dim(m1)
 # library(magrittr)
 # m1 %>%
 #   as.array()
+
+all_boolean <- function(x) {
+  # convert tensor of 1s and 0s to a unique boolean
+  as.logical(torch$all(x)$numpy())
+}
+
+
+# testing that new generic all, any for tensors work
+A <- torch$ones(60000L, 1L, 28L, 28L)
+C <- A * 0.5
+all(torch$lt(C, A))
+all(C < A)
+all(A < C)
+
+
+all(torch$tensor(list(1, 1, 1)))
+
+all(torch$tensor(list(1, 1, 0)))
+
+any(torch$tensor(list(1, 1, 0)))
+
+any(torch$tensor(list(0, 0, 0)))
+
+all(torch$eye(3L))
+any(torch$eye(3L))
