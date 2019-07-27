16b80095f772d8613662c33446eab4b2b9cb66c5
f0nzie <alfonso.reyes@oilgainsanalytics.com>
2019-07-26 19:05:23 -0500

    add example for all_dims()

--- R/extract.R
+++ R/extract.R
@@ -259,6 +259,18 @@ torch_extract_opts <- function(
 #' # to translate the above python expression to R, write:
 #' x[all_dims(), i]
 #' }
+#' @examples
+#' # Run this
+#' d <- torch$tensor(list(list(0, 0),
+#'                        list(0, 0),
+#'                        list(0, 1),
+#'                        list(1, 1)), dtype=torch$uint8)
+#' d[all_dims(), 1]
+#'
+#' f <- torch$arange(9L)$reshape(c(3L, 3L))
+#' f
+#' f[all_dims()]
+#' f[all_dims(), 1L]
 all_dims <- function()
   .globals$builtin_ellipsis %||%
   (.globals$builtin_ellipsis <- import_builtins(FALSE)$Ellipsis)
