9e83063088eb6e26819d96624a165ac27dbb314f
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2019-08-01 19:54:33 -0500

    rename script datasets.R to python_functions.R. make dataset_mnist_digits() visible in Reference.

--- R/python_functions.R
+++ R/python_functions.R
@@ -1,5 +1,3 @@
-
-
 #' MNIST database of handwritten digits
 #'
 #' Dataset of 60,000 28x28 grayscale images of the 10 digits, along with a test
@@ -8,14 +6,12 @@
 #' @param ntrain number of training samples
 #' @param ntest number of test samples
 #' @param onehot boolean
-#' @family datasets
 #'
 #' @export
+#'
 dataset_mnist_digits <- function(ntrain = 60000L, ntest = 10000L, onehot = TRUE) {
-    # import data_utils module
-    python_path <- system.file("python", package = "rTorch")
-    tools <- import_from_path("torchtools", path = python_path)
-    tools$data_util$load_mnist(ntrain, ntest, onehot)
+  # import data_utils module
+  python_path <- system.file("python", package = "rTorch")
+  tools <- import_from_path("torchtools", path = python_path)
+  tools$data_util$load_mnist(ntrain, ntest, onehot)
 }
-
-
