37383a0e55c72ce32cfce06639722ae3b0f51709
og-analytics <og-analytics@oilgainsanalytics.com>
2020-08-12 15:51:34 -0500

    add test custom function `expect_all_true()`

--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -2,3 +2,7 @@ skip_if_no_torch <- function() {
   if (!reticulate::py_module_available("torch"))
     skip("Torch not available for testing")
 }
+
+expect_all_true <- function(obj, ...) {
+    testthat::expect_true(all(object = obj), ...)
+}
