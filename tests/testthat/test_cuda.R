0116797303dbc09dff4ea0b9f63bf2bd66220242
f0nzie <alfonso.reyes@oilgainsanalyitics.com>
2021-10-23 17:42:21 +0800

    test if "cuda" can be selected to torch$device

--- tests/testthat/test_cuda.R
+++ tests/testthat/test_cuda.R
@@ -9,3 +9,12 @@ test_that("CUDA is available", {
     # print(torch$cuda$is_available())
     expect_equal(torch$cuda$is_available(), TRUE)
 })
+
+test_that("device is CUDA selected", {
+    torch_object <- c("torch.device")
+    two_objects  <- c("torch.device", "python.builtin.object")
+    dev <- torch$device(ifelse(torch$cuda$is_available(), "cuda", "cpu"))
+
+    expect_true(class(dev)[1] %in% torch_object)
+    expect_equal(class(dev), two_objects)
+})
